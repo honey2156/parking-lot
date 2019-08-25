0ab7882d5bde92a2e6e7194c494f2778a340f1ba
honey2156 <honey2156@gmail.com>
2019-08-26 01:59:31 +0530

    declared constants for commands

--- src/main/java/io/gojek/parkinglot/App.java
+++ src/main/java/io/gojek/parkinglot/App.java
@@ -1,13 +1,11 @@
 package io.gojek.parkinglot;
 
 /**
- * Hello world!
+ * @author Mandeep Singh
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+
+	}
 }
---
+++ src/main/java/io/gojek/parkinglot/constants/Commands.java
@@ -0,0 +1,17 @@
+package io.gojek.parkinglot.constants;
+
+/**
+ * @author Mandeep Singh
+ *
+ */
+public interface Commands {
+
+	public static final String CREATE_PARKING_LOT = "create_parking_lot";
+	public static final String PARK = "park";
+	public static final String LEAVE = "leave";
+	public static final String STATUS = "status";
+	public static final String REG_NUMBER_FOR_CARS_WITH_COLOR = "registration_numbers_for_cars_with_colour";
+	public static final String SLOTS_NUMBER_FOR_CARS_WITH_COLOR = "slot_numbers_for_cars_with_colour";
+	public static final String SLOTS_NUMBER_FOR_REG_NUMBER = "slot_number_for_registration_number";
+
+}
